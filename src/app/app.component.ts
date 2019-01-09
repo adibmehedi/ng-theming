d72b67fef0951e4e239850eda63406753caa5460
adibmehedi <adibmehedi@gmail.com>
2019-01-09 23:30:26 +0600

    sidebar small screen auto hide added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -8,8 +11,16 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app';
   themeName = 'gryffindor';
-  constructor(){
-    
+
+  constructor( 
+    private breakpointObserver: BreakpointObserver){
   }
+
+  isHandset: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => {
+        return result.matches;
+      })
+    );
 
 }
